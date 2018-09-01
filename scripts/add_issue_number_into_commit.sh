6fdf8c954c2c08dc57050ba5cfbbfbcdaeb0180a
jinyowo <hjin421@gmail.com>
2018-09-01 13:59:24 +0900

    [#2] 이슈브랜치에서만 prepare-commit-msg 쉘이 동작하도록 로직 수정

--- scripts/add_issue_number_into_commit.sh
+++ scripts/add_issue_number_into_commit.sh
@@ -2,6 +2,9 @@ branch_name=`git rev-parse --abbrev-ref HEAD`
 issue_number=`echo ${branch_name} | cut -d '-' -f1`
 first_line=`head -n1 ${$1}`
 
-if [ -z "$first_line" ]; then
-    sed -i ".bak" "1s/^/[#$issue_number] /" $1
+check_number=${issue_number//[0-9]/}
+if [ -z "$check_number" ]; then
+   if [ -z "$first_line" ]; then
+       sed -i ".bak" "1s/^/[#$issue_number] /" $1
+   fi
 fi
