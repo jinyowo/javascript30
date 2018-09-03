fb32ff07046a89f4a8a9098798b36fb93e9668ae
jinyowo <hjin421@gmail.com>
2018-09-03 16:55:00 +0900

    [#7] 시계바늘에 바운스 추가
    - resetTransitionEffect: 바늘이 한바퀴를 돌아 각도가 리셋될때, 한바퀴 움직이는 모습이 transition때문에 보이는 이슈때문에 추가

--- 2-day/script.js
+++ 2-day/script.js
@@ -17,6 +17,7 @@ function setHours(hour, min) {
 	const hourHand = document.querySelector('.hour-hand');
 	let deg = (hour / 12) * 360 + 90 + (min / 60) * 30;
 
+	resetTransitionEffect(deg, 90, hourHand); // 23:59 ~ 00:00 이 되는 시점
 	hourHand.style.transform = `rotate(${deg}deg)`;
 }
 
@@ -24,6 +25,7 @@ function setMinutes(min) {
 	const minHand = document.querySelector('.min-hand');
 	let deg = (min / 60) * 360 + 90;
 
+	resetTransitionEffect(min, 0, minHand); // **:59 ~ **:00 이 되는 시점
 	minHand.style.transform = `rotate(${deg}deg)`;
 }
 
@@ -31,5 +33,14 @@ function setSeconds(second) {
 	const secondHand = document.querySelector('.second-hand');
 	let deg = (second / 60) * 360 + 90;
 
+	resetTransitionEffect(second, 0, secondHand); // **:**:59 ~ **:**:00 이 되는 시점
 	secondHand.style.transform = `rotate(${deg}deg)`;
+}
+
+function resetTransitionEffect(value, compare, hand) {
+	if (value === compare) {
+		hand.classList.remove('transition-ing');
+	} else if (!hand.classList.contains('transition-ing') && value > compare) {
+		hand.classList.add('transition-ing');
+	}
 }
