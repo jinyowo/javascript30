e5b64658aee87a74cfee03fef6a5bd695461f073
jinyowo <hjin421@gmail.com>
2018-12-07 16:56:10 +0900

    [#9] Update RangeControl moudle & CSS file

--- 3-day/script.js
+++ 3-day/script.js
@@ -2,61 +2,19 @@ class RangeControl {
     constructor() {
         this._init();
         this._attachEvent();
-        this._setDefaultOption();
     }
 
     _init() {
-        this._colorTitle = document.querySelector('h1 .js');
-        this._img = document.querySelector('.main-img');
-        this._spacingInput = document.querySelector('input.spacing');
-        this._blurInput = document.querySelector('input.blur');
-        this._colorInput = document.querySelector('input.color');
-
-        this._spacingSizing = this._spacingInput.getAttribute('data-sizing');
-        this._blurSizing = this._blurInput.getAttribute('data-sizing');
-    }
-
-    _setDefaultOption() {
-        this._setSpacing();
-        this._setBlur();
-        this._setColor();
+        this._inputContainer = document.querySelector('.controls');
     }
 
     _attachEvent() {
-        this._spacingInput.addEventListener('input', this._onInputSpacing.bind(this));
-        this._blurInput.addEventListener('input', this._onInputBlur.bind(this));
-        this._colorInput.addEventListener('input', this._onInputColor.bind(this));
+        this._inputContainer.addEventListener('input', this._onInputChange.bind(this));
     }
 
-    _onInputSpacing(e) {
-        if (e.target !== this._spacingInput) return;
-
-        this._setSpacing();
-    }
-
-    _onInputBlur(e) {
-        if (e.target !== this._blurInput) return;
-
-        this._setBlur();
-    }
-
-    _onInputColor(e) {
-        if (e.target !== this._colorInput) return;
-
-        this._setColor();
-    }
-
-    _setSpacing() {
-        this._img.style.padding = `${this._spacingInput.value}${this._spacingSizing}`;
-    }
-
-    _setBlur() {
-        this._img.style.filter = `blur(${this._blurInput.value}${this._blurSizing})`;
-    }
-
-    _setColor() {
-        this._colorTitle.style.color = this._colorInput.value;
-        this._img.style.backgroundColor = this._colorInput.value;
+    _onInputChange(e) {
+        const subfix = e.target.dataset.sizing || '';
+        document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value + subfix);
     }
 }
 
